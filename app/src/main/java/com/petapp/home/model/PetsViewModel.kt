4384f53694ccab5766f3d0ea49e30b0c2bcd5046
Victor Jimbei <victor.jimbei.gemini@yummly.com>
2024-01-25 07:02:08 +0200

    Added empty screen.

--- app/src/main/java/com/petapp/home/model/PetsViewModel.kt
+++ app/src/main/java/com/petapp/home/model/PetsViewModel.kt
@@ -1,19 +1,24 @@
 package com.petapp.home.model
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.view.View
+import androidx.lifecycle.AndroidViewModel
+import com.petapp.R
 import com.petapp.domain.GetPetsUseCase
 import com.petapp.home.listener.OnPetClickListener
 import com.petapp.home.mapper.PetToPetUiMapper
 import com.petapp.home.state.PetsUiState
 import com.petapp.util.livedata.NonNullLiveData
+import com.petapp.util.livedata.SingleLiveEvent
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import javax.inject.Inject
 
-class PetsViewModel @Inject constructor(val getPetsUseCase: GetPetsUseCase) : ViewModel(), OnPetClickListener {
+class PetsViewModel @Inject constructor(val app: Application, val getPetsUseCase: GetPetsUseCase) : AndroidViewModel(app), OnPetClickListener {
 
     private val disposables = CompositeDisposable()
-    val uiState = NonNullLiveData<PetsUiState>(PetsUiState.LoadingPetsUiState(true, emptyList(), ""))
+    val uiState = NonNullLiveData<PetsUiState>(PetsUiState.LoadingPetsUiState(true, emptyList(), "", View.VISIBLE))
+    val navigateToDetailsScreen = SingleLiveEvent<Void>()
 
     init {
         getPetsData()
@@ -24,7 +29,10 @@ class PetsViewModel @Inject constructor(val getPetsUseCase: GetPetsUseCase) : Vi
     }
 
     fun onSwipeToRefresh() {
-        uiState.value = PetsUiState.LoadingPetsUiState(pets = uiState.value.pets)
+        uiState.value = PetsUiState.LoadingPetsUiState(
+            pets = uiState.value.pets,
+            emptyPlaceholderVisibility = if (uiState.value.pets.isEmpty()) View.VISIBLE else View.GONE,
+        )
         getPetsData()
     }
 
@@ -34,11 +42,16 @@ class PetsViewModel @Inject constructor(val getPetsUseCase: GetPetsUseCase) : Vi
             .map(PetToPetUiMapper())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
-                { uiState.value = PetsUiState.PetsLoadedUiState(pets = it) },
+                {
+                    uiState.value = PetsUiState.PetsLoadedUiState(
+                        pets = it,
+                        emptyPlaceholderVisibility = if (it.isEmpty()) View.VISIBLE else View.GONE
+                    )
+                },
                 {
                     uiState.value = PetsUiState.PetsErrorUiState(
                         pets = uiState.value.pets,
-                        errorMessage = it.stackTraceToString()
+                        errorMessage = app.getString(R.string.empty_message)
                     )
                 }
             ).let { disposables.add(it) }
